0082751051e9387d60b4fe1b9f850767f769b3c1
Cho Won Jae <yoteamo7@naver.com>
2020-10-26 18:03:40 +0900

    logout page 구현

--- server/index.js
+++ server/index.js
@@ -98,7 +98,10 @@ app.get('/api/users/auth', auth, (req, res)=>{
         isAdmin : req.user.role === 0 ? false : true,
         isAuth : true,
         email : req.user.email,
-        name : req.user.name
+        name : req.user.name,
+        lastname : req.user.lastname,
+        role : req.user.role,
+        image : req.user.image
     })
 })
 
